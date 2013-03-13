3330f39b930a015e3a770cbb5372020632b09d41
Kenny Tran <unique@kennyletran.com>
2013-03-12 21:23:30 -0700

    changed pandora mode to jarvis

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -175,10 +175,10 @@ $(function(){
   $('.radio-button').click(function(){
     if ($('.testing1').attr('data-radio') == 'true') {
       $('.testing1').attr('data-radio', 'false')
-      $('.radio-button').html('Pandora Mode is Off')
+      $('.radio-button').html('Jarvis is off')
     } else {
       $('.testing1').attr('data-radio', 'true')
-      $('.radio-button').html('Pandora Mode is On')
+      $('.radio-button').html('Jarvis is on')
     }
   })
 
@@ -486,7 +486,7 @@ $(function(){
 
   $('.about').click(function(){
     $('#songwrap').empty()
-    $('#songwrap').append('<div class="about-text"><i>"she got a big booty so I call her big booty"</i> <br> - Two Chainz <br><br> we aspire to be that simple.<br><br><i>"they ask me what I do and who I do it fo"</i><br>-Two Chainz<br><br>we do it because we think the people who share good music<br>are the most awesome people in the world<br><br>turn pandora mode on and after every song finishes,<br>the player will play a random song on the left.<br>this is true even if you are browsing pages<br><br>on the song list, notice the "&" numbers.<br> type it in a remark and it will turn into a link<br> for example:  <span id="song"><a href="songs?d=6jhC6GjGC5M&aboutus">&25</a></span><br><br>straight magical. <br><br><br>the ^ button gives the song another point.<br>^ buttons are anonymous<br><br>you are now a master<br>turn pandora mode on and relax<br>.roseay</div>')
+    $('#songwrap').append('<div class="about-text"><i>"she got a big booty so I call her big booty"</i> <br> - Two Chainz <br><br> we aspire to be that simple.<br><br><i>"they ask me what I do and who I do it fo"</i><br>-Two Chainz<br><br>we do it because we think the people who share good music<br>are the most awesome people in the world<br><br>turn Jarvis on and after every song finishes,<br>Jarvis will play a random song on the left.<br>Jarvis is smart enough to know when you change the page<br>Jarvis loves you<br><br>on the song list, notice the "&" numbers.<br> type it in a remark and it will turn into a link<br> for example:  <span id="song"><a href="songs?d=6jhC6GjGC5M&aboutus">&25</a></span><br><br>straight magical. <br><br><br>the ^ button gives the song another point.<br>^ buttons are anonymous<br><br>you are now a master<br>turn pandora mode on and relax<br>.roseay</div>')
   })
 
   setInterval(function(){
