252c33cb1a17256a7281f709f7189bd97d084a9a
Kenny Tran <unique@kennyletran.com>
2013-03-20 16:59:30 -0700

    turned on autoplay

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -195,7 +195,7 @@ $(function(){
   }
 
   function onPlayerReady(event){
-    event.target.pauseVideo();
+    event.target.playVideo();
   }
   
   var createRadioTooltip = function(){
@@ -494,7 +494,6 @@ $(function(){
     var link   = this['href'].split('songs?d=')[1]
     var songId = $(this).parent().parent().attr('id')
     $('.testing1').attr('data-youtube-code', link + '?autoplay=1&controls=1&iv_load_policy=3&autohide=1&modestbranding=1&vq=hd360')
-    $('.testing1').attr('data-next-song', 'KHYJH_V-94s?autoplay=0&controls=1&iv_load_policy=3&autohide=1&modestbranding=1&vq=hd360')
     $('.testing1').attr('data-song-played', songId);
 
     if (link == undefined){
